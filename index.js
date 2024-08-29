5247a7c9d6bd3327f6f6b32e75cf948af844ceff
osanchezGSU <150194031+osanchezGSU@users.noreply.github.com>
2024-08-29 08:45:51 -0400

    Added Animation on scroll to service cards

--- index.js
+++ index.js
@@ -5,3 +5,53 @@ $(window).on("scroll", () =>{
         $(".nav-bar").removeClass("bottom-border");
     }
 })
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0)
+
+const cards = document.querySelectorAll('.service-card');
+
+cards.forEach(card => {
+    const cardOveraly = card.querySelector('.card-overlay');
+    const cardContent = card.querySelectorAll('.card-content');
+
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: card,
+            start: '25% 75%',
+            end: '50% 70%',
+            scrub: true,
+            markers: true
+        }
+    })
+    
+    tl.fromTo(cardOveraly, {
+        opacity: 0
+    },
+    {
+        opacity: 1
+    },
+    '<' );
+    tl.fromTo(cardContent, {
+        opacity: 0
+    },
+    {
+        opacity: 1
+    },
+    '<' );
+
+})
+
+
+
